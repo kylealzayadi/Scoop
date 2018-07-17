451c3b44dca277d903b0263b1d845bb1e1e80802
kyleStark <kyle.a.show@gmail.com>
2018-07-16 18:06:53 -0700

    commit#?

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,31 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
-  constructor(public navCtrl: NavController, private camera: Camera) {
-  }
   myphoto:any;
-    takePhoto(){
-      const options: CameraOptions = {
-        quality: 100,
-        destinationType: this.camera.DestinationType.FILE_URI,
-        encodingType: this.camera.EncodingType.JPEG,
-        mediaType: this.camera.MediaType.PICTURE
-      }
-      
-      this.camera.getPicture(options).then((imageData) => {
-       // imageData is either a base64 encoded string or a file URI
-       // If it's base64 (DATA_URL):
-       this.myphoto = 'data:image/jpeg;base64,' + imageData;
-      }, (err) => {
-       // Handle error
-      });
+  options;
+  constructor(public navCtrl: NavController, private camera: Camera, public sanitizer: DomSanitizer) {
+    this.myphoto= "https://www.collinsdictionary.com/images/thumb/duck_170690246_250.jpg?version=3.1.158";
+    this.options =  {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
     }
   }
+  takePhoto(){
+  
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      console.log(imageData);
+      console.log('data:image/jpeg;base64' + imageData)
+      // this.myphoto = 'data:image/jpeg;base64,' + imageData;
+      this.myphoto = 'data:image/jpeg;base64' + imageData
+      console.log(this.myphoto);
+    }, (err) => {
+      // Handle error
+    });
+  }
+}
 
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+declare var google: any;
 import {
   GoogleMaps,
   GoogleMap,
@@ -8,7 +9,7 @@ import {
   Marker
 } from '@ionic-native/google-maps';
 import { Platform } from 'ionic-angular';
-import { Component} from "@angular/core/";
+import { Component } from "@angular/core/";
 import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
@@ -19,35 +20,238 @@ export class HomePage {
   map;
   lat;
   long;
-  constructor( public platform: Platform, private geolocation: Geolocation) {
+  constructor(public platform: Platform, private geolocation: Geolocation) {
     platform.ready().then(() => {
       console.log('loading map')
 
 
-        this.loadMap();
+      this.loadMap();
     });
-}
+  }
 
   loadMap() {
     const options = {
-      enableHighAccuracy : true
+      enableHighAccuracy: true,
+
     }
 
     this.geolocation.getCurrentPosition(options).then((resp) => {
       this.lat = +resp.coords.latitude;
       this.long = +resp.coords.longitude;
-      
+
       let mapOptions: GoogleMapOptions = {
         camera: {
-           target: {
-             lat: this.lat,
-             lng: this.long
-           },
-           zoom: 18,
-           tilt: 30,
-         }
-      };
+          target: {
+            lat: this.lat,
+            lng: this.long
+          },
+          zoom: 18,
+          tilt: 30,
 
+        },
+        styles:
+          [
+            {
+              "featureType": "administrative",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.country",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#d60000"
+                },
+                {
+                  "visibility": "simplified"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.land_parcel",
+              "elementType": "labels",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.neighborhood",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#df1114"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.province",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#ee2023"
+                }
+              ]
+            },
+            {
+              "featureType": "landscape.man_made",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#fff1ad"
+                }
+              ]
+            },
+            {
+              "featureType": "landscape.natural",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#3ebf03"
+                }
+              ]
+            },
+            {
+              "featureType": "landscape.natural.landcover",
+              "elementType": "labels.text",
+              "stylers": [
+                {
+                  "color": "#000ecc"
+                }
+              ]
+            },
+            {
+              "featureType": "landscape.natural.terrain",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#ffc533"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "elementType": "labels.text",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.medical",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#00d7eb"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#efe22e"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.place_of_worship",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#c51b8f"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.sports_complex",
+              "elementType": "labels.text",
+              "stylers": [
+                {
+                  "visibility": "on"
+                }
+              ]
+            },
+            {
+              "featureType": "road",
+              "elementType": "labels.icon",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#123aed"
+                }
+              ]
+            },
+            {
+              "featureType": "road.local",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#ffeb33"
+                },
+                {
+                  "weight": 8
+                }
+              ]
+            },
+            {
+              "featureType": "road.local",
+              "elementType": "labels",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "transit",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.station.rail",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#945900"
+                }
+              ]
+            }
+          ]
+      };
+        var markerLocations=[
+          [34.0224, 118.2851]
+        ]
+      
+
+        
       this.map = GoogleMaps.create('map_canvas', mapOptions);
 
       let marker: Marker = this.map.addMarkerSync({
@@ -59,10 +263,22 @@ export class HomePage {
           lng: this.long,
         }
       });
+      
+          {
+          marker = new google.maps.Marker({
+           map: this.map,
+          title: 'usc',
+          icon: 'blue',
+          animation: 'DROP',
+          position: {
+            lat: 34.0224,
+            lng: 118.2851,
+          }
+        })};
       marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
         alert('clicked');
       });
-  
+
     }).catch((error) => {
       console.log('Error getting location', error);
     });
