ba230ba47ab6daf345ba5d7f804e094b43eb31f3
kyleStark <kyle.a.show@gmail.com>
2018-07-17 10:49:17 -0700

    cameraworks

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { DomSanitizer } from '@angular/platform-browser';
+import { normalizeURL } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -10,11 +11,11 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class AboutPage {
   myphoto:any;
   options;
-  constructor(public navCtrl: NavController, private camera: Camera, public sanitizer: DomSanitizer) {
+  constructor(public navCtrl: NavController, private camera: Camera, private sanitizer: DomSanitizer) {
     this.myphoto= "https://www.collinsdictionary.com/images/thumb/duck_170690246_250.jpg?version=3.1.158";
     this.options =  {
       quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
+      destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     }
@@ -24,12 +25,13 @@ export class AboutPage {
     this.camera.getPicture(this.options).then((imageData) => {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64 (DATA_URL):
-      console.log(imageData);
-      console.log('data:image/jpeg;base64' + imageData)
+
       // this.myphoto = 'data:image/jpeg;base64,' + imageData;
-      this.myphoto = 'data:image/jpeg;base64' + imageData
-      console.log(this.myphoto);
+      let img = normalizeURL(imageData);
+      console.log(img);
+      this.myphoto = this.sanitizer.bypassSecurityTrustResourceUrl(img);
     }, (err) => {
+      console.log(err);
       // Handle error
     });
   }
